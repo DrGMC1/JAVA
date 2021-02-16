97004713caab3d899078c4e5e4680cb49835bc92
DrGMC <47373280+DrGMC@users.noreply.github.com>
2021-02-16 07:54:18 -0500

    Add files via upload

---
+++ Chapter 03/BlockIfStatements.java
@@ -0,0 +1,21 @@
+package WeekThree;
+
+public class BlockIfStatements {
+
+public static void main(String[] args) {
+
+	 { 
+	        String stingofwords = "THIS IS SO COOL "; 
+	        int y = 3; 
+	  
+	        // if block 
+	        if (y == 4) {
+	            y++; 
+	            System.out.println(stingofwords); 
+	        }
+	  
+	        // Executed by default 
+	        System.out.println("the original value of y is = " + y); 
+	    } 
+	} 
+}
---
+++ Chapter 03/SimpleIf.java
@@ -0,0 +1,16 @@
+package WeekThree;
+
+public class SimpleIf {
+
+	public static void main( String[] args )
+	{
+		if( 1 > 1 ); 
+			System.out.println( "Five is greater than one." );		
+
+		if( 2 < 4 )
+		
+			System.out.println( "Two is less than four." );
+			System.out.println( "Test succeeded." );
+	}
+	
+}
