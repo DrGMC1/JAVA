6e38d4d8ca8409ae841a9919bc88d99725376afc
DrGMC <47373280+DrGMC@users.noreply.github.com>
2021-03-16 09:18:10 -0400

    Add files via upload

---
+++ Chapter 07/exampleDosArrays.java
@@ -0,0 +1,14 @@
+package week7;
+
+public class exampleDosArrays {
+	  public static void main(String[] args) {
+	    int [] userAge;
+	    userAge = new int [5] ;
+	    userAge [3] = 31;
+	    System.out.println(userAge [0]);
+	    System.out.println(userAge [1]);
+	    System.out.println(userAge [2]);
+	    System.out.println(userAge [3]);
+	    System.out.println(userAge [4]);
+	  }
+	}
---
+++ Chapter 07/exampleOneArrays.java
@@ -0,0 +1,11 @@
+package week7;
+
+public class exampleOneArrays {
+	  public static void main(String[] args) {
+	    int [] userAge;
+	   userAge = new int [] {1,2,3};  //  
+	
+	    // userAge = new int [5] ;
+	    System.out.println(userAge [2]);
+	  }
+	}
