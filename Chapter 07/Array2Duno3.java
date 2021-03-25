54af5953833cfd7e6dab05f7e2110166c7c1f269
DrGMC <47373280+DrGMC@users.noreply.github.com>
2021-03-25 10:20:48 -0400

    Add files via upload

---
+++ Chapter 07/Array2Duno3.java
@@ -0,0 +1,16 @@
+package week7;
+
+public class Array2Duno3 {
+
+	public static void  main (String [] args) {
+		
+		int[][] numbers = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+	
+		System.out.println("these are the elements:" + numbers [0][0]);
+		System.out.println("these are the elements:" + numbers [1][1]);
+		System.out.println("these are the elements:" + numbers [2][2]);
+		System.out.println();
+	    System.out.println("1st row : " + numbers[2].length);
+	}
+	
+}
