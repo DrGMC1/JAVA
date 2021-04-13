6c048d4aa401bf41bf7db046369c7f29d15c9f23
DrGMC <47373280+DrGMC@users.noreply.github.com>
2021-04-13 08:35:37 -0400

    Add files via upload

---
+++ Inheritance/ConstructorDemo1.java
@@ -0,0 +1,15 @@
+package weekPointTwo;
+
+/**
+This program demonstrates the order in which
+superclass and subclass constructors are called.
+*/
+
+public class ConstructorDemo1
+{
+public static void main(String[] args)
+{
+   SubClass1 itsClass = new SubClass1();// This statement declares the variable "obj"
+   								//CREATES a "Subclass1" Object and passess nothing
+}
+}
---
+++ Inheritance/SuperClass1.java
@@ -0,0 +1,14 @@
+package weekPointTwo;
+
+public class SuperClass1
+{
+   /**
+      Constructor
+   */
+
+   public SuperClass1()
+   {
+      System.out.println("This is the " +
+               "superclass constructor.");
+   }
+}
