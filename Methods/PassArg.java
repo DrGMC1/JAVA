60145b01d66ebe069a475cd091aed3536c98ac07
Dr. Munoz @ github <47373280+DrGMC@users.noreply.github.com>
2022-03-18 07:17:15 -0400

    Add files via upload

---
+++ Methods/PassArg.java
@@ -0,0 +1,34 @@
+package WeekSix;
+/**
+   This program demonstrates a method with a parameter.
+*/
+
+public class PassArg
+{
+   public static void main(String[] args)
+   {
+      int x = 10;
+      double d = 1.0;
+
+      System.out.println("I am passing values to displayValue.");
+      displayValue(5);                       // Pass 5
+      displayValue(x);                       // Pass 10
+      displayValue(x * 4-2);                   // Pass 40
+      //displayValue(Integer.parseInt("700")); // Pass 700
+      displayValueDouble(d);                  //Pass double
+      System.out.println("Now I am back in main.");
+   }
+   
+/**      The displayValue method displays the value
+      of its integer parameter.*/
+
+   public static void displayValue(int num1)
+   {
+      System.out.println("The value is " + num1);
+   }
+   
+   public static void displayValueDouble(double d)
+   {
+      System.out.println("The value is " + d);
+   }
+}
